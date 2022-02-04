9c11bde3fc6422fc276094d405951e05bf350e66
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-02-04 16:25:23 -0300

    exercicios de 1 a 7 dia 4.2

---
+++ fundamentos/bloco-04-java-script-primeiros-passos/dia-4.2-javascript-array-e-loop_for/1-ao-7.js
@@ -0,0 +1,70 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index]);
+// } // exercicio 1
+
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+// }
+
+// console.log(soma); // exercicio 2
+
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+// }
+// let media = soma / numbers.length;
+// console.log(media); // exercicio 3
+
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+// }
+// let media = soma / numbers.length;
+
+// if (media > 20) {
+//   console.log('Valor maior que 20');
+// }
+// else {
+//   console.log('Valor menor ou igual a 20');
+// } // exercicio 4
+
+// let maiorNumero = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] > maiorNumero) {
+//     maiorNumero = numbers[index];
+//   }
+// }
+
+// console.log(maiorNumero); // exercicio 5
+
+// let quantidadeDeImpares = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 === 1) {
+//     quantidadeDeImpares += 1;
+//   }
+// }
+
+// if (quantidadeDeImpares > 0) {
+//   console.log(quantidadeDeImpares);
+// }
+// else {
+//   console.log('Nenhum valor ímpar encontrado');
+// } // exercicio 6
+
+// let menorNumero = numbers[numbers.length -1];
+
+// for (let index = numbers.length - 1; index >= 0; index -= 1) {
+//   if (numbers[index] <= menorNumero) {
+//     menorNumero = numbers[index];
+//   }
+// }
+
+// console.log(menorNumero); // exercicio 7
