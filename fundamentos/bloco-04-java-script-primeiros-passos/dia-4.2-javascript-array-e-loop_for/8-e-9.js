b14d0135bb8d2afbf3ed7afa34444f01de188a73
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-02-04 19:35:45 -0300

    exercicios 8 e 9

---
+++ fundamentos/bloco-04-java-script-primeiros-passos/dia-4.2-javascript-array-e-loop_for/8-e-9.js
@@ -0,0 +1,10 @@
+let newArray = [];
+
+for (let index = 1; index <= 25; index += 1) {
+    newArray.push(index);
+}
+console.log(newArray); // exercicio 8
+
+for (let index2 = 0; index2 < newArray.length; index2 +=1) {
+    console.log(newArray[index2] / 2);
+} // exercicio 9
