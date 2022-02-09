b91a3067b0228a8f90ea648529d879d9ec95bd9e
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-02-09 16:15:16 -0300

    fixacao

---
+++ fundamentos/bloco-05-java-script-dom-eventos-e-web_storage/dia-5.1-dom-e-seletores/exercicios/script.js
@@ -0,0 +1,8 @@
+let header = document.querySelector('header');
+header.style.background = 'green';
+
+var emergencyClass = document.querySelector('.emergency-tasks');
+emergencyClass.style.background = 'lightcoral';
+
+var noEmergencyClass = document.querySelectorAll('.no-emergency-tasks')[0];
+noEmergencyClass.style.background = 'orange';
