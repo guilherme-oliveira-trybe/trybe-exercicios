0658438abce61ee7872e9f89983f91072a661197
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-02-10 22:05:16 -0300

    parte 1 exercicios

--- fundamentos/bloco-05-java-script-dom-eventos-e-web_storage/dia-5.2-java-script-trabalhando-com-elementos/fixacao/script.js
+++ fundamentos/bloco-05-java-script-dom-eventos-e-web_storage/dia-5.2-java-script-trabalhando-com-elementos/fixacao/script.js
@@ -1,4 +1,4 @@
-console.log(document.getElementById('elementoOndeVoceEsta'));
+// console.log(document.getElementById('elementoOndeVoceEsta'));
 
 let pai = document.getElementById('elementoOndeVoceEsta').parentNode;
 
@@ -8,15 +8,15 @@ let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
 
 primeiroFilhoDoFilho.innerHTML = 'adicione um texto a ele';
 
-console.log(document.querySelector('#pai').firstElementChild);
+// console.log(document.querySelector('#pai').firstElementChild);
 
-console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling);
+// console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling);
 
-console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
+// console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
 
-console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling);
+// console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling);
 
-console.log(document.querySelector('#pai').children[2]);
+// console.log(document.querySelector('#pai').children[2]);
 
 let paiDeTodos = document.querySelector('#pai');
 
@@ -24,7 +24,7 @@ let irmaoDoElementoOndeVoceEsta = document.createElement('section');
 
 paiDeTodos.appendChild(irmaoDoElementoOndeVoceEsta);
 
-console.log(document.querySelector('#pai').children);
+// console.log(document.querySelector('#pai').children);
 
 let paiElementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
 
@@ -32,7 +32,7 @@ let terceiroFilhoDoFilho = document.createElement('section');
 
 paiElementoOndeVoceEsta.appendChild(terceiroFilhoDoFilho);
 
-console.log(document.querySelector('#elementoOndeVoceEsta').children)
+// console.log(document.querySelector('#elementoOndeVoceEsta').children)
 
 let paiPrimeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
 
@@ -43,3 +43,7 @@ paiPrimeiroFilhoDoFilho.appendChild(primeiroFilhoDoPaiPrimeiroFilhoDoFilho);
 console.log(document.querySelector('#primeiroFilhoDoFilho').children);
 
 console.log(document.querySelector('#primeiroFilhoDoFilho').children[0].parentNode.parentNode.nextElementSibling);
+
+let father = document.querySelector('#paiDoPai').children;
+
+
