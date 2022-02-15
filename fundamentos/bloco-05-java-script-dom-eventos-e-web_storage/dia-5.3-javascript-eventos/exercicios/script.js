a2ce89e3bd941f963f126b6c3a4440ac17136f05
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-02-14 23:01:17 -0300

    Adiciona função troca cor do background dos feriados

--- fundamentos/bloco-05-java-script-dom-eventos-e-web_storage/dia-5.3-javascript-eventos/exercicios/script.js
+++ fundamentos/bloco-05-java-script-dom-eventos-e-web_storage/dia-5.3-javascript-eventos/exercicios/script.js
@@ -15,10 +15,10 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-const listDays = document.querySelector('#days');
-
 function createListDezDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const listDays = document.querySelector('#days');
+
   for (let index = 0; index < dezDaysList.length; index += 1) {
     const dezDays = dezDaysList[index];
     const dezDaysListItem = document.createElement('li');
@@ -45,5 +45,36 @@ function addClassLi(array, classes) {
 addClassLi(['24', '25', '31'], 'holiday');
 
 addClassLi(['4', '11', '18', '25'], 'friday');
+
+function createButton () {
+  const buttonsContainer = document.querySelector('.buttons-container');
+  const button = document.createElement('button');
+  button.innerHTML = 'Feriados';
+  button.id = 'btn-holiday';
+  buttonsContainer.appendChild(button);
+}
+
+createButton();
+
+const button = document.querySelector('#btn-holiday');
+let isHoliday = true;
+
+function changeBackgroundColor () {
+  const holiday = document.querySelectorAll('.holiday');
+
+  if (isHoliday) {
+    for (let index = 0; index < holiday.length; index += 1) {
+      holiday[index].style.background = 'red';
+    }  
+  }else {
+    for (let index = 0; index < holiday.length; index += 1) {
+      holiday[index].style.background = 'rgb(238,238,238)';
+    }
+  }
+  isHoliday = !isHoliday;
+}
+
+button.addEventListener('click', changeBackgroundColor);
+
 
 
